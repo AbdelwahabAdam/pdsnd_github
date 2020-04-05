f9a9ac7b818b81eb1cae306e80f6689e29c19f5e
Abdelwahab Osama <hopaasd@outlook.com>
2020-04-05 02:22:44 +0200

    fix city comment and add filter comment
    sever commit

--- bikeshare.py
+++ bikeshare.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import statistics as st
 
-////loading data
+###loading data
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+###choose the right data function
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
